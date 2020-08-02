0049c4176829c423f2ffc00f0ef50db32499ae46
Luis Mantilla <luis.mantilla@evertecinc.com>
2020-08-02 17:03:46 -0400

    Add mockito plugin and MockMoviesRepository

---
+++ test/features/movies/domain/usecases/get_popular_movies_test.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/mockito.dart';
+import 'package:popular_movies/features/movies/domain/repositories/movies_repository.dart';
+
+/// This represents the mock for the MoviesRepository
+class MockMoviesRepository extends Mock implements MoviesRepository {}
